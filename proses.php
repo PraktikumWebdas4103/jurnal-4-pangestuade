9ccabe975bfee1aff4c0f4687760db900a35adc6
Ade Pangestu <43510254+pangestuade@users.noreply.github.com>
2018-09-25 16:55:39 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,16 @@
 <?php
-
-
+if(isset($_POST['submit'])){
+	foreach($_POST['hob'] as $item){
+      
+      echo $item ."<br/>";
+}
+}
 ?>
+<?php
+if (isset($_POST['submit'])){
+$fileName = $_FILES['gambar']['nama'];
+// Simpan di Folder Gambar
+move_uploaded_file($_FILES['gambar']['tmp_name'], "gambar/".$_FILES['gambar']['nama']);
+echo"<script>alert('Gambar Berhasil diupload !');history.go(-1);</script>";   
+}
+?> 
